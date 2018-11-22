5228ad8d15bc086cb8e6269d5dcedf1deced9722
Kar <Pratik_Kar@Dell.com>
2018-11-22 17:32:37 +0530

    OK

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,4 +1,30 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String welcomeMessage){
+        this.welcomeMessage=welcomeMessage;
+    }
+
+    public String getWelcomeMessage() {
+        return welcomeMessage;
+    }
+
+    public void setWelcomeMessage(String welcomeMessage) {
+        this.welcomeMessage = welcomeMessage;
+    }
+
+
+    private String welcomeMessage;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return welcomeMessage;
+    }
 }
