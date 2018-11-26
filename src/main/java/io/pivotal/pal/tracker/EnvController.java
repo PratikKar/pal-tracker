a03c33c117f78259b79b36be8c3bb362b33f65d1
Kar <Pratik_Kar@Dell.com>
2018-11-26 13:09:06 +0530

    git lab 2

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,6 +1,5 @@
 package io.pivotal.pal.tracker;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,69 +10,32 @@ import java.util.Map;
 @RestController
 public class EnvController {
 
-
-
-    private String  port;
-    private String memoryLimit;
-    private String cfInstanceIndex;
-    private String cfInstanceAddr;
+    private final String port;
+    private final String memoryLimit;
+    private final String cfInstanceIndex;
+    private final String cfInstanceAddress;
 
     public EnvController(
             @Value("${PORT:NOT SET}") String port,
-            @Value("${MEMORY_LIMIT :NOT SET }") String memoryLimit,
-            @Value("${CF_INSTANCE_INDEX :NOT SET }") String cfInstanceIndex,
-            @Value("${CF_INSTANCE_ADDR :NOT SET }") String cfInstanceAddr) {
+            @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit,
+            @Value("${CF_INSTANCE_INDEX:NOT SET}") String cfInstanceIndex,
+            @Value("${CF_INSTANCE_ADDR:NOT SET}") String cfInstanceAddress
+    ) {
         this.port = port;
         this.memoryLimit = memoryLimit;
         this.cfInstanceIndex = cfInstanceIndex;
-        this.cfInstanceAddr = cfInstanceAddr;
+        this.cfInstanceAddress = cfInstanceAddress;
     }
 
     @GetMapping("/env")
-    public Map<String, String> getEnv() throws Exception {
-        Map<String, String > map =new HashMap<String, String>();
-        map.put("CF_INSTANCE_INDEX",this.getCfInstanceIndex());
-        map.put("CF_INSTANCE_ADDR",this.getCfInstanceAddr());
-        map.put("PORT",this.getPort());
-        map.put("MEMORY_LIMIT",this.getMemoryLimit());
-    return map;
+    public Map<String, String> getEnv() {
+        Map<String, String> env = new HashMap<>();
+
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memoryLimit);
+        env.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        env.put("CF_INSTANCE_ADDR", cfInstanceAddress);
+
+        return env;
     }
-
-
-    public String getCfInstanceAddr() {
-        return cfInstanceAddr;
-    }
-
-    public void setCfInstanceAddr(String cfInstanceAddr) {
-        this.cfInstanceAddr = cfInstanceAddr;
-    }
-
-
-    public String  getPort() {
-        return port;
-    }
-
-    public void setPort(String port) {
-        this.port = port;
-    }
-
-    public String getMemoryLimit() {
-        return memoryLimit;
-    }
-
-    public void setMemoryLimit(String memoryLimit) {
-        this.memoryLimit = memoryLimit;
-    }
-
-    public String getCfInstanceIndex() {
-        return cfInstanceIndex;
-    }
-
-    public void setCfInstanceIndex(String cfInstanceIndex) {
-        this.cfInstanceIndex = cfInstanceIndex;
-    }
-
-
-
-
 }
